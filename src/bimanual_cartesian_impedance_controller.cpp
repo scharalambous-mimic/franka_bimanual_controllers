6dba7f251b2e4fb271ab6bb92723d24c7e24d9aa
Stefanos Charalambous <stefanos.charalambous@mimicrobotics.com>
2025-07-28 17:33:05 +0200

    Revert "refactor: add clipping helper function to avoid duplicate code"
    This reverts commit b5268220e8152306b6fb7bf57d37afe1b7d35ba8.

--- src/bimanual_cartesian_impedance_controller.cpp
+++ src/bimanual_cartesian_impedance_controller.cpp
@@ -23,15 +23,6 @@
 #include <std_srvs/SetBool.h>
 namespace franka_bimanual_controllers {
 
-template <typename Derived>
-void clipVectorMagnitude(Eigen::MatrixBase<Derived>& vec, const double limit) {
-    const double magnitude = vec.norm();
-    // Only scale the vector if its magnitude is non-zero and exceeds the limit.
-    if (magnitude > limit && magnitude > 0) {
-        vec *= (limit / magnitude);
-    }
-}
-
 bool BiManualCartesianImpedanceControl::initArm(
     hardware_interface::RobotHW* robot_hw,
     const std::string& arm_id,
@@ -349,7 +340,9 @@ void BiManualCartesianImpedanceControl::updateArmLeft() {
   Eigen::Affine3d transform_right(Eigen::Matrix4d::Map(robot_state_right.O_T_EE.data()));
   Eigen::Map<Eigen::Matrix<double, 7, 1>> dq_right(robot_state_right.dq.data());
   Eigen::Vector3d position_right(transform_right.translation());
-
+  // left_arm_data.position_other_arm_=position_right;
+  // compute error to desired pose
+  // position error
   geometry_msgs::PoseStamped msg_left;
   msg_left.pose.position.x=position[0];
   msg_left.pose.position.y=position[1];
@@ -385,21 +378,27 @@ void BiManualCartesianImpedanceControl::updateArmLeft() {
   force_torque_msg.wrench.torque.z=left_arm_data.force_torque[5];
   pub_force_torque_left.publish(force_torque_msg);
 
-  // compute error to desired pose
-  // position error
   Eigen::Matrix<double, 6, 1> error_left;
   error_left.head(3) << position - left_arm_data.position_d_;
-  // clip the position error
-  clipVectorMagnitude(error_left.head(3), delta_lim);
 
+  // calculate the magnitude of the position error
+  double position_error_magnitude = error_left.head(3).norm();
+  if (position_error_magnitude > delta_lim){
+    // scale the position error to the delta_lim
+    error_left.head(3) *= (delta_lim / position_error_magnitude);
+  }
 
-  // relative position error
   Eigen::Matrix<double, 6, 1> error_relative;
   error_relative.head(3) << position - position_right;
   error_relative.tail(3).setZero();
   error_relative.head(3)<< error_relative.head(3) -left_arm_data.position_d_relative_;
-  // clip the relative position error
-  clipVectorMagnitude(error_relative.head(3), delta_lim);
+
+  // calculate the magnitude of the relative position error
+  double relative_error_magnitude = error_relative.head(3).norm();
+  if (relative_error_magnitude > delta_lim){
+    // scale the relative position error to the delta_lim
+    error_relative.head(3) *= (delta_lim / relative_error_magnitude);
+  }
 
   // orientation error
   if (left_arm_data.orientation_d_.coeffs().dot(orientation.coeffs()) < 0.0) {
@@ -411,10 +410,16 @@ void BiManualCartesianImpedanceControl::updateArmLeft() {
   Eigen::AngleAxisd error_quaternion_angle_axis(error_quaternion);
   // compute "orientation error"
   error_left.tail(3) << error_quaternion_angle_axis.axis() * error_quaternion_angle_axis.angle();
+
+
   // define orientation error clipping limit
   const double orientation_delta_lim = delta_lim * 3.0;
-  // clip the orientation error
-  clipVectorMagnitude(error_left.tail(3), orientation_delta_lim);
+  // calculate the magnitude of the orientation error
+  double orientation_error_magnitude = error_left.tail(3).norm();
+  if (orientation_error_magnitude > orientation_delta_lim) {
+    // scale the orientation error to the orientation_delta_lim
+    error_left.tail(3) *= (orientation_delta_lim / orientation_error_magnitude);
+  }
 
   // compute control
   // allocate variables
@@ -534,6 +539,17 @@ void BiManualCartesianImpedanceControl::updateArmRight() {
   Eigen::Affine3d transform_left(Eigen::Matrix4d::Map(robot_state_left.O_T_EE.data()));
   Eigen::Map<Eigen::Matrix<double, 7, 1>> dq_left(robot_state_left.dq.data());
   Eigen::Vector3d position_left(transform_left.translation());
+  // compute error to desired pose
+  // position error
+  Eigen::Matrix<double, 6, 1> error_right;
+  error_right.head(3) << position - right_arm_data.position_d_;
+
+  // calculate the magnitude of the position error
+  double position_error_magnitude = error_right.head(3).norm();
+  if (position_error_magnitude > delta_lim){
+    // scale the position error to the delta_lim
+    error_right.head(3) *= (delta_lim / position_error_magnitude);
+
 
   geometry_msgs::PoseStamped msg_right;
   msg_right.pose.position.x=position[0];
@@ -569,20 +585,17 @@ void BiManualCartesianImpedanceControl::updateArmRight() {
   force_torque_msg.wrench.torque.z=right_arm_data.force_torque[5];
   pub_force_torque_right.publish(force_torque_msg);
 
-  // compute error to desired pose
-  // position error
-  Eigen::Matrix<double, 6, 1> error_right;
-  error_right.head(3) << position - right_arm_data.position_d_;
-  // clip the position error
-  clipVectorMagnitude(error_right.head(3), delta_lim);
-
-  // relative position error
   Eigen::Matrix<double, 6, 1> error_relative;
   error_relative.head(3) << position - position_left;
   error_relative.tail(3).setZero();
   error_relative.head(3)<< error_relative.head(3) -right_arm_data.position_d_relative_;
-  // clip the relative position error
-  clipVectorMagnitude(error_relative.head(3), delta_lim);
+
+  // calculate the magnitude of the relative position error
+  double relative_error_magnitude = error_relative.head(3).norm();
+  if (relative_error_magnitude > delta_lim){
+    // scale the relative position error to the delta_lim
+    error_relative.head(3) *= (delta_lim / relative_error_magnitude);
+  }
   
   // orientation error
   if (right_arm_data.orientation_d_.coeffs().dot(orientation.coeffs()) < 0.0) {
@@ -594,11 +607,15 @@ void BiManualCartesianImpedanceControl::updateArmRight() {
   Eigen::AngleAxisd error_quaternion_angle_axis(error_quaternion);
   // compute "orientation error"
   error_right.tail(3) << error_quaternion_angle_axis.axis() * error_quaternion_angle_axis.angle();
+
   // define orientation error clipping limit
   const double orientation_delta_lim = delta_lim * 3.0;
-  // clip the orientation error
-  clipVectorMagnitude(error_right.tail(3), orientation_delta_lim)
-  
+  // calculate the magnitude of the orientation error
+  double orientation_error_magnitude = error_right.tail(3).norm();
+  if (orientation_error_magnitude > orientation_delta_lim) {
+    // scale the orientation error to the orientation_delta_lim
+    error_right.tail(3) *= (orientation_delta_lim / orientation_error_magnitude);
+  }
 
   // compute control
   // allocate variables
